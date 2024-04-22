9acdaada690f542071180e61e5a54cb124ac95e6
Sediq <sediqullah.kismatkhan@powercoders.org>
2024-04-22 15:45:35 +0200

    JS exercises

---
+++ Javascript/Exercise_5/script.js
@@ -0,0 +1,7 @@
+let birthYear = 1995;
+let futureYear = 2025;
+
+let result = futureYear - birthYear;
+let result_2 = result - 1;
+
+alert("I will be either " + result + " or " + result_2 + " in " + futureYear + ".");
